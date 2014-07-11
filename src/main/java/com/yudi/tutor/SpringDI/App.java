e585747531405ad2386aaf77a611b4541af0ab70
Yudi Wiradinata <yudiwiradinata@gmail.com>
2014-07-11 10:56:41 +0700

    fors commit

---
+++ src/main/java/com/yudi/tutor/SpringDI/App.java
@@ -0,0 +1,13 @@
+package com.yudi.tutor.SpringDI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
